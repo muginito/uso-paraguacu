84f9f34ea452d509cea7abbe0cfa564bce8b46ef
muginito <miguel.vramospro@gmail.com>
2024-11-06 15:17:20 -0300

    Troca nomes de arquivos
    Entrypoint.py -> Home.py
    Home.py -> Tabela

--- Entrypoint.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([st.Page("Home.py"), st.Page("Mapa.py")])
-pg.run()
--- Home.py
+++ Home.py
@@ -1,26 +1,4 @@
 import streamlit as st
-import pandas as pd
 
-def filtrar(df, label, coluna):
-    filtro = st.multiselect(label, df[coluna].unique())
-    if filtro:
-        return df[df[coluna].isin(filtro)]
-    return df
-
-st.title("Uso Paraguaçu")
-
-url = "https://raw.githubusercontent.com/muginito/uso-paraguacu/refs/heads/main/dados/USO_PARAGUACU.CSV"
-
-df = pd.read_csv(url)
-
-filtro_cidades = filtrar(df, "Cidades", "MUNICIPIO")
-
-st.dataframe(filtro_cidades,
-             width=1000,
-             height=700,
-             column_config={
-                "Ano": st.column_config.NumberColumn(format="%d"),
-                "Codigo": st.column_config.NumberColumn(format="%d")
-             },
-             hide_index=True
-             )
+pg = st.navigation([st.Page("Tabela.py", title="Home"), st.Page("Mapa.py")])
+pg.run()
